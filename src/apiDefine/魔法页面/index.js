2de75cef444d19e4edfba0f712091a33aa0c605f
Jamter Tsai <121978614@qq.com>
2018-03-30 21:59:53 +0800

    fix(接口)：修改格式

--- src/apiDefine/魔法页面/index.js
+++ src/apiDefine/魔法页面/index.js
@@ -7,41 +7,43 @@ var apis = [
             pageSize: 10,
             page: 0
         },
-        data: [{
-            "id": 16,
-            "name": "16年模版",
-            "previewList": [
-                {
-                    "componentName": "Banner",
-                    "props": {
-                        "imgSrc": "https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=3448484253,3685836170&fm=27&gp=0.jpg"
+        data: {
+            list: [{
+                "id": 16,
+                "name": "16年模版",
+                "previewList": [
+                    {
+                        "componentName": "Banner",
+                        "props": {
+                            "imgSrc": "https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=3448484253,3685836170&fm=27&gp=0.jpg"
+                        }
+                    },
+                    {
+                        "componentName": "Input",
+                        "props": {
+                            "placeholder": "请输入要搜索的商品"
+                        }
                     }
-                },
-                {
-                    "componentName": "Input",
-                    "props": {
-                        "placeholder": "请输入要搜索的商品"
+                ]
+            }, {
+                "id": 17,
+                "name": "17年模版",
+                "previewList": [
+                    {
+                        "componentName": "Banner",
+                        "props": {
+                            "imgSrc": "https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=3448484253,3685836170&fm=27&gp=0.jpg"
+                        }
+                    },
+                    {
+                        "componentName": "Input",
+                        "props": {
+                            "placeholder": "请输入要搜索的商品"
+                        }
                     }
-                }
-            ]
-        }, {
-            "id": 17,
-            "name": "17年模版",
-            "previewList": [
-                {
-                    "componentName": "Banner",
-                    "props": {
-                        "imgSrc": "https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=3448484253,3685836170&fm=27&gp=0.jpg"
-                    }
-                },
-                {
-                    "componentName": "Input",
-                    "props": {
-                        "placeholder": "请输入要搜索的商品"
-                    }
-                }
-            ]
-        }]
+                ]
+            }]
+        }
     }, {
         desc: '接口2： 根据id删除指定模版',
         url: '/api/magic/template/delete/id',
